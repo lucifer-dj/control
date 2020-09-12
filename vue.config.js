80b9eeb545b0fc214b00416c44d977358d8a2a3c
look-dj <look-dj@outlook.com>
2020-09-13 03:41:55 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -52,7 +52,7 @@ module.exports = {
     //是否为 CSS 开启 source map。设置为 true 之后可能会影响构建的性能。
     sourceMap: false,
     //是否将组件中的 CSS 提取至一个独立的 CSS 文件中 (而不是动态注入到 JavaScript 中的 inline 代码)。
-    extract: false,
+    extract: true,
     //默认情况下，只有 *.module.[ext] 结尾的文件才会被视作 CSS Modules 模块。设置为 true 后你就可以去掉文件名中的 .module 并将所有的 *.(css|scss|sass|less|styl(us)?) 文件视为 CSS Modules 模块。
     requireModuleExtension: false,
     //向 CSS 相关的 loader 传递选项。
